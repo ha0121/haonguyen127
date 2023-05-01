0e8ffa5385e586298eee59d1caa6d57ba93b974a
hao0121 <truykichany@gmail.com>
2023-05-01 15:15:22 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0E1112',
+        grey: '#484B4B',
+        secondary: '#19C778',
+
+        accent: '#EEF7F9',
+      },
+    },
+  },
+  plugins: [],
+}
